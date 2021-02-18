4ab31899031d3842ad151c396e028169faccbb1b
Eduardo <eduardopoox.99@gmail.com>
2021-02-18 17:03:28 -0600

    Agregar archivos de proyecto.

---
+++ Poox-Facturacion/Controllers/AccessController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using Poox_Facturacion.Models;
+
+namespace Poox_Facturacion.Controllers
+{
+    public class AccessController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login(string email, string password)
+        {
+            try
+            {
+                using (facturacionEntities db= new facturacionEntities())
+                {
+                    var oUser = from d in db.user
+                                where d.email == email && d.idState == 1
+                                select d;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Content("Ocurrio un error");
+            }
+        }
+
+    }
+}
---
+++ Poox-Facturacion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Poox_Facturacion.MvcApplication" Language="C#" %>
